50352ae35472c26ed0e375ca8ccc1361916b6132
Avanishdubey1707 <avanishdubeymsd@gmail.com>
2025-09-30 13:45:23 +0530

    model traning and evaluation

--- src/utils.py
+++ src/utils.py
@@ -4,8 +4,9 @@ import numpy as np
 import pandas as pd 
 import dill
 
-from src.exception import CustomExecption
 
+from src.exception import CustomExecption
+from sklearn.metrics import r2_score
 def save_object(file_path,obj):
     try:
         dir_path=os.path.dirname(file_path)
@@ -17,3 +18,16 @@ def save_object(file_path,obj):
 
     except Exception as e:
         raise CustomExecption(e,sys)
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+        for model_name, model in models.items():  # loop with both name and model
+            model.fit(X_train, y_train)
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            test_model_score = r2_score(y_test, y_test_pred)
+            report[model_name] = test_model_score
+        return report
+    except Exception as e:
+        raise CustomExecption(e, sys)
